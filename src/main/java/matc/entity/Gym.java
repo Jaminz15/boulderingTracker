f92c3e90aec6b8f069fdf966775d13bdaa524d21
Jamin <jzimmerman15@madisoncollege.edu>
2025-02-17 01:50:40 -0600

    add Gym entity with JPA annotations and attribute mappings

--- src/main/java/matc/entity/Gym.java
+++ src/main/java/matc/entity/Gym.java
@@ -1,4 +1,29 @@
 package matc.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+/**
+ * A class to represent a Gym.
+ */
+@Entity
+@Table(name = "gym")
 public class Gym {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
