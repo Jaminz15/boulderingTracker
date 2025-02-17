cab6a422293a4535be2d63afab064efd2bd98e56
Jamin <jzimmerman15@madisoncollege.edu>
2025-02-17 02:04:05 -0600

    added getters, setters, and toString method for Gym

--- src/main/java/matc/entity/Gym.java
+++ src/main/java/matc/entity/Gym.java
@@ -2,9 +2,6 @@ package matc.entity;
 
 import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 /**
@@ -26,23 +23,106 @@ public class Gym {
 
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
+
+    /**
+     * Instantiates a new Gym.
+     */
+    public Gym() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    /**
+     * Instantiates a new Gym.
+     *
+     * @param name     the name of the gym
+     * @param location the location of the gym
+     */
+    public Gym(String name, String location) {
+        this.name = name;
+        this.location = location;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets location.
+     *
+     * @return the location
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * Sets location.
+     *
+     * @param location the location
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * Gets created at.
+     *
+     * @return the created at
+     */
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    /**
+     * Sets created at.
+     *
+     * @param createdAt the created at
+     */
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Gym{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
 
-/**
- * Instantiates a new Gym.
- */
-public Gym() {
-    this.createdAt = LocalDateTime.now();
-}
-
-/**
- * Instantiates a new Gym.
- *
- * @param name     the name of the gym
- * @param location the location of the gym
- */
-public Gym(String name, String location) {
-    this.name = name;
-    this.location = location;
-    this.createdAt = LocalDateTime.now();
-}
