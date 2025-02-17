1caaf94da8814bc99b2568a22118d854416a5951
Jamin <jzimmerman15@madisoncollege.edu>
2025-02-17 02:14:44 -0600

    added Climb entity with JPA annotations and attribute mappings

--- src/main/java/matc/entity/Climb.java
+++ src/main/java/matc/entity/Climb.java
@@ -1,4 +1,39 @@
 package matc.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+import java.time.LocalDate;
+
+/**
+ * A class to represent a Climb.
+ */
+@Entity
+@Table(name = "climb")
 public class Climb {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "gym_id", nullable = false)
+    private Gym gym;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "climb_type", nullable = false)
+    private String climbType;
+
+    @Column(name = "grade", nullable = false)
+    private String grade;
+
+    @Column(name = "attempts", nullable = false)
+    private int attempts;
+
+    @Column(name = "success", nullable = false)
+    private boolean success;
+
+    @Column(name = "notes")
+    private String notes;
 }
