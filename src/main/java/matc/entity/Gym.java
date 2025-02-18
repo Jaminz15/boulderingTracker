7052f523ab0b4405884e65d19b35bb1c52c571d9
Jamin <jzimmerman15@madisoncollege.edu>
2025-02-17 23:22:21 -0600

    Added one-to-many relationship between Gym and Climb

--- src/main/java/matc/entity/Gym.java
+++ src/main/java/matc/entity/Gym.java
@@ -3,6 +3,7 @@ package matc.entity;
 import jakarta.persistence.*;
 import org.hibernate.annotations.GenericGenerator;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * A class to represent a Gym.
@@ -24,6 +25,9 @@ public class Gym {
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
+    @OneToMany(mappedBy = "gym", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Climb> climbs;
+
     /**
      * Instantiates a new Gym.
      */
@@ -42,6 +46,8 @@ public class Gym {
         this.location = location;
         this.createdAt = LocalDateTime.now();
     }
+
+    // Getters and Setters
 
     /**
      * Gets id.
@@ -115,6 +121,24 @@ public class Gym {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Gets climbs.
+     *
+     * @return the climbs
+     */
+    public List<Climb> getClimbs() {
+        return climbs;
+    }
+
+    /**
+     * Sets climbs.
+     *
+     * @param climbs the climbs
+     */
+    public void setClimbs(List<Climb> climbs) {
+        this.climbs = climbs;
+    }
+
     @Override
     public String toString() {
         return "Gym{" +
@@ -122,6 +146,7 @@ public class Gym {
                 ", name='" + name + '\'' +
                 ", location='" + location + '\'' +
                 ", createdAt=" + createdAt +
+                ", climbs=" + climbs +
                 '}';
     }
 }
