1456c14c9ae456a89cb832c1060cf812a2748041
Jamin <jzimmerman15@madisoncollege.edu>
2025-02-27 22:55:14 -0600

    Implement POST request to add a new gym

--- src/main/java/matc/controller/GymManagement.java
+++ src/main/java/matc/controller/GymManagement.java
@@ -33,4 +33,17 @@ public class GymManagement extends HttpServlet {
         RequestDispatcher dispatcher = req.getRequestDispatcher("/gymManagement.jsp");
         dispatcher.forward(req, resp);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+
+        if ("add".equals(action)) {
+            String gymName = req.getParameter("gymName");
+            String gymLocation = req.getParameter("gymLocation");
+            Gym newGym = new Gym(gymName, gymLocation);
+            gymDao.insert(newGym);
+        }
+        resp.sendRedirect("gymManagement");
+    }
 }
