235e48818dc160936a55548a6e606351d0d0cad4
Jamin <jzimmerman15@madisoncollege.edu>
2025-02-27 22:23:01 -0600

    Initialize DAO for GymManagement and setup servlet

---
+++ src/main/java/matc/controller/GymManagement.java
@@ -0,0 +1,28 @@
+package matc.controller;
+
+import matc.entity.Gym;
+import matc.persistence.GenericDao;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Gym Management Controller - Handles listing, adding, and deleting gyms.
+ */
+@WebServlet("/gymManagement")
+public class GymManagement extends HttpServlet {
+    private GenericDao<Gym> gymDao;
+
+    /**
+     * Initializes the DAO for database access.
+     */
+    @Override
+    public void init() throws ServletException {
+        gymDao = new GenericDao<>(Gym.class);
+    }
+}
