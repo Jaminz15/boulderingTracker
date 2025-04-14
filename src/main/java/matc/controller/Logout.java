8b04d333c8cfa178ae001785e56f1cdcdeae888c
Jamin <jzimmerman15@madisoncollege.edu>
2025-04-13 22:03:27 -0500

    add info log for user logout in Logout servlet

--- src/main/java/matc/controller/Logout.java
+++ src/main/java/matc/controller/Logout.java
@@ -3,12 +3,16 @@ package matc.controller;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+import org.apache.logging.log4j.*;
 import java.io.IOException;
 
 @WebServlet("/logout")
 public class Logout extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger(Logout.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("User logged out");
         // Invalidate session
         req.getSession().invalidate();
 
