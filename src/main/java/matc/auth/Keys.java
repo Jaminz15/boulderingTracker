d56dac325c793560e68251731ba99f826742e12e
Jamin <jzimmerman15@madisoncollege.edu>
2025-05-08 21:32:42 -0500

    add Javadoc comments to Keys.java for imporved clarity

--- src/main/java/matc/auth/Keys.java
+++ src/main/java/matc/auth/Keys.java
@@ -4,11 +4,24 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
+/**
+ * Keys - Represents a collection of JSON Web Keys (JWKS) retrieved from AWS Cognito.
+ * Holds a list of public keys used for verifying JWT signatures.
+ */
 public class Keys{
 
+	/**
+	 * A list of KeysItem objects representing individual public keys
+	 * retrieved from the JWKS endpoint.
+	 */
 	@JsonProperty("keys")
 	private List<KeysItem> keys;
 
+	/**
+	 * Retrieves the list of public keys used for JWT signature validation.
+	 *
+	 * @return a list of KeysItem objects
+	 */
 	public List<KeysItem> getKeys(){
 		return keys;
 	}
