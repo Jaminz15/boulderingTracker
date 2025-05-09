b8c63922246f28196364a38ba2439b70dd62d4f9
Jamin <jzimmerman15@madisoncollege.edu>
2025-05-08 21:31:41 -0500

    add Javadoc comments to CognitoTokenHeader.java for imporved clarity

--- src/main/java/matc/auth/CognitoTokenHeader.java
+++ src/main/java/matc/auth/CognitoTokenHeader.java
@@ -2,18 +2,34 @@ package matc.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * CognitoTokenHeader - Represents the header portion of a JWT token from AWS Cognito.
+ * Contains key ID (kid) and algorithm (alg) used for signing.
+ */
 public class CognitoTokenHeader{
 
+	// The Key ID used to identify the public key for token validation.
 	@JsonProperty("kid")
 	private String kid;
 
+	// The algorithm used to sign the JWT.
 	@JsonProperty("alg")
 	private String alg;
 
+	/**
+	 * Gets the Key ID (kid) from the JWT header.
+	 *
+	 * @return the Key ID as a String
+	 */
 	public String getKid(){
 		return kid;
 	}
 
+	/**
+	 * Gets the algorithm (alg) used to sign the JWT.
+	 *
+	 * @return the algorithm as a String
+	 */
 	public String getAlg(){
 		return alg;
 	}
