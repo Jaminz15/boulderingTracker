d67000bd0e9b46af728841979b8fa8b139e001d1
Jamin <jzimmerman15@madisoncollege.edu>
2025-05-08 21:41:06 -0500

    add Javadoc comments to RestApplication.java for imporved clarity

--- src/main/java/matc/rest/RestApplication.java
+++ src/main/java/matc/rest/RestApplication.java
@@ -8,26 +8,44 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 
 import java.util.*;
 
+/**
+ * RestApplication - Configures the JAX-RS application for the BoulderBook project.
+ * Sets up REST endpoints and registers context resolvers and features.
+ * Uses the /services path for all RESTful APIs.
+ */
 @ApplicationPath("/services")
 public class RestApplication extends Application {
 
+    /**
+     * Registers the classes that will be used as JAX-RS resources and providers.
+     *
+     * @return a set of resource and provider classes
+     */
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classes = new HashSet<>();
+        // Register RESTful web service and JSON feature classes
         classes.add(GymWebService.class);
         classes.add(JacksonFeature.class);
         classes.add(ObjectMapperContextResolver.class);
         return classes;
     }
 
+    /**
+     * Registers singleton instances that are used as context resolvers.
+     * Configures a custom ObjectMapper for JSON serialization.
+     *
+     * @return a set of singleton objects used for context resolution
+     */
     @Override
     public Set<Object> getSingletons() {
         Set<Object> singletons = new HashSet<>();
 
         singletons.add((javax.ws.rs.ext.ContextResolver<ObjectMapper>) type -> {
+            // Create a custom ObjectMapper for handling JSON with Java 8 date/time support
             ObjectMapper mapper = new ObjectMapper();
             mapper.registerModule(new JavaTimeModule());
-            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // <- Optional, nice formatting
+            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
             return mapper;
         });
 
