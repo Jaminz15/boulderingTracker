7acbf1bcf0dae66eefd94e943c30f48fcbe4410e
Jamin <jzimmerman15@madisoncollege.edu>
2025-05-08 21:18:02 -0500

    add Javadoc comments to Logout.java for imporved clarity

--- src/main/java/matc/controller/Logout.java
+++ src/main/java/matc/controller/Logout.java
@@ -6,10 +6,23 @@ import javax.servlet.http.*;
 import org.apache.logging.log4j.*;
 import java.io.IOException;
 
+/**
+ * Logout servlet to handle user logout and session invalidation.
+ * Redirects the user to the AWS Cognito logout endpoint after clearing the session.
+ */
 @WebServlet("/logout")
 public class Logout extends HttpServlet {
     private static final Logger logger = LogManager.getLogger(Logout.class);
 
+    /**
+     * Handles GET requests to log out the user.
+     * Invalidates the user session and redirects to the AWS Cognito logout URL.
+     *
+     * @param req  the HttpServletRequest object
+     * @param resp the HttpServletResponse object
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an input or output error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         logger.info("User logged out");
